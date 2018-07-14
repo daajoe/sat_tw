b2a7cea11cbc0eecf149a93eedceebec7f7277ee
Johannes Fichte <daajoe@ficht.es>
2018-07-14 19:05:53 +0200

    Added primal graph generation for SAT

--- gen_primal.py
+++ gen_primal.py
@@ -10,9 +10,14 @@ import backports.lzma as xz
 
 import mimetypes
 
-from itertools import imap
+from itertools import imap, combinations
 
-def read_fromfile(filename):
+import cStringIO
+import networkx as nx
+
+
+def read_fromfile(filename, output):
+    graph = nx.Graph()
     try:
         stream = None
         mtype = mimetypes.guess_type(filename)[1]
@@ -27,6 +32,7 @@ def read_fromfile(filename):
         else:
             raise IOError('Unknown input type "%s" for file "%s"' % (mtype, filename))
         numVariables = 0
+        numEdges = 0
         for line in stream:
             line = line.split()
             if line == [] or line[0] in ('c', 'n'):
@@ -34,10 +40,27 @@ def read_fromfile(filename):
             if line[0] == 'p':
                 num_vars = int(line[2])
                 num_cls = int(line[3])
-                print 'p tw %s %s'%(num_vars,num_cls)
             else:
-                print ' '.join(imap(lambda x: str(abs(int(x))), line[:-1]))
+                #hypergraph
+                #print ' '.join(imap(lambda x: str(abs(int(x))), line[:-1]))
+                for u,v in combinations(line[:-1],2):
+                    numEdges+=1
+                    #output.write("%s %s\n" %(abs(int(u)),abs(int(v))))
+                    graph.add_edge(abs(int(u)),abs(int(v)))
 
+        numEdges-=1
+        nu_graph=nx.convert_node_labels_to_integers(graph,first_label=1)
+        #nu_graph = graph
+
+        output.write('p tw %s %s\n'%(nu_graph.number_of_nodes(),nu_graph.number_of_edges()))
+        #print numEdges
+        for u,v in nu_graph.edges():
+            output.write("%s %s\n" %(u,v))
+        #output.seek(0)
+        #output.write('p tw %s %s\n'%(num_vars,numEdges))
+            
+        print output.getvalue()[:-1],
+                    
     finally:
         if stream:
             stream.close()
@@ -55,8 +78,8 @@ def parse_args():
 def main():
     args = parse_args()
     filename = args.instance
-    print filename
-    read_fromfile(filename)
+    output = cStringIO.StringIO()
+    read_fromfile(filename, output)
 
 
 main()
